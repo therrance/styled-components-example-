76f0756110b4550543dbef0935678d1632382247
Taras Gordiienko <lozuvatka@gmail.com>
2021-12-02 15:17:39 +0200

    add inheritance

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 300px;
-  margin: 70px auto 0;
 `;
 
-const Input = styled.input`
-  padding: ${(props) => (props["data-id"] === "name" ? "10px" : "14px")};
-  border-radius: 4px;
-  border: 1px solid ${(props) => props.borderColor};
-  otline: none;
-  background: #1b1c23;
+const Title = styled(Wrapper)`
+  color: #8857d8;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Input
-        readOnly
-        placeholder="Your name"
-        borderColor="white"
-        data-id="name"
-        onClick={(event) => alert(event.target.dataset.id)}
-      />
+      <Title>Hello</Title>
+      {/* <Content color="red" hide>
+        Simple example
+      </Content> */}
     </Wrapper>
   );
 }
