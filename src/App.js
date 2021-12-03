261ff0b0191a4c5e5f0f9eda49093b2f294463c7
Taras Gordiienko <lozuvatka@gmail.com>
2021-12-03 16:30:03 +0200

    add nesting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+const Thing = styled.div`
+  color: blue;
+  position: relative;
+  width: 200px;
+  left: 20px;
+  cursor: pointer;
 
-const Content = styled.div`
-  font-size: 24px;
-`;
+  span {
+    color: yellow;
 
-const StyledTitle = styled(Title)`
-  color: #8857d8;
-`;
+    b {
+      color: red;
 
-function Title(props) {
-  return <div className={props.className}>{props.children}</div>;
-}
+      i {
+        color: blue;
+      }
+    }
+  }
+`;
 
 function App() {
   return (
-    <Wrapper>
-      <StyledTitle as="h1">Hello</StyledTitle>
-      <Content color="red" hide>
-        Simple example
-      </Content>
-    </Wrapper>
+    <Thing>
+      Hello World!
+      <span>
+        span{" "}
+        <b>
+          bold <i>cursive</i>
+        </b>
+      </span>
+    </Thing>
   );
 }
 
