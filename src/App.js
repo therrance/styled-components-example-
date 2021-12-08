5cb69108c95f5cc821e2e56555c46d9753b4425e
therrance <lozuvatka@gmail.com>
2021-12-08 14:30:47 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
 import styled from "styled-components";
+import "./App.css";
 
 const Thing = styled.div`
-  color: blue;
   position: relative;
   width: 200px;
-  left: 20px;
+  left: 80px;
   cursor: pointer;
+  padding: 20px 30px;
+  border-radius: 5px;
+  margin-top: 5px;
+  transition: all 300ms;
+  
+  // Псевдоселектор
+  &:hover {
+    text-indent: 5px; // <Thing> при наведении
+  }
+  
+  // <Thing> + <Thing>, Второй по счету Thing
+  & + & {
+    background: #0e85ff;
+  }
+  
+  // <Thing> вместе с классом ".something"
+  &.something {
+    background: orange;
+  }
+  
+  // Псевдоэлементы
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    top: 5px;
+    left: -60px;
+    width: 50px;
+    height: 50px;
+    background: #0e85ff;
+    border-radius: 50%;
+  }
 
-  span {
-    color: yellow;
-
-    b {
-      color: red;
-
-      i {
-        color: blue;
-      }
-    }
+  &::after {
+    left: unset;
+    right: -60px;
   }
 `;
 
 function App() {
   return (
-    <Thing>
-      Hello World!
-      <span>
-        span{" "}
-        <b>
-          bold <i>cursive</i>
-        </b>
-      </span>
-    </Thing>
+    <>
+      <Thing className="thing[[
+        \
+        ">Hello World!</Thing>
+    </>
   );
 }
 
