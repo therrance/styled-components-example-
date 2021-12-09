ace0efd8d03ea02eb92ad4a2b81e909147d032aa
therrance <lozuvatka@gmail.com>
2021-12-09 08:50:53 +0200

    add componenet call

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
 import styled from "styled-components";
-import "./App.css";
 
-const Thing = styled.div`
-  position: relative;
-  width: 200px;
-  left: 80px;
-  cursor: pointer;
-  padding: 20px 30px;
-  border-radius: 5px;
-  margin-top: 5px;
-  transition: all 300ms;
-  
-  // Псевдоселектор
-  &:hover {
-    text-indent: 5px; // <Thing> при наведении
-  }
-  
-  // <Thing> + <Thing>, Второй по счету Thing
-  & + & {
-    background: #0e85ff;
-  }
-  
-  // <Thing> вместе с классом ".something"
-  &.something {
-    background: orange;
-  }
-  
-  // Псевдоэлементы
-  &::before,
-  &::after {
-    content: "";
-    position: absolute;
-    top: 5px;
-    left: -60px;
-    width: 50px;
-    height: 50px;
-    background: #0e85ff;
-    border-radius: 50%;
-  }
+const Parent = styled.div`
+  font-size: 25px;
+  display: flex;
+  align-items: center;
+`;
 
-  &::after {
-    left: unset;
-    right: -60px;
+const Child = styled.div`
+  width: 20px;
+  height: 20px;
+  background: #0e85ff;
+  border-radius: 50%;
+
+  ${Parent}:hover & {
+    background: yellow;
   }
 `;
 
 function App() {
   return (
-    <>
-      <Thing className="thing[[
-        \
-        ">Hello World!</Thing>
-    </>
+    <Parent>
+      Наведите сюда
+      <Child />
+    </Parent>
   );
 }
 
